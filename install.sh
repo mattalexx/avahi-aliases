b6e902dccc2289ee40710a2fc9ee7483ee013989
Matt Alexander <mattalexx@gmail.com>
2012-02-12 18:58:58 -0700

    Do not use sudo

--- install.sh
+++ install.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
+
+if [ "$(id -u)" != "0" ]; then
+	echo "This script must be run as root" 1>&2
+	exit 1
+fi
+
 SCRIPT_PATH=${0%/*}
 . $SCRIPT_PATH/lib/coloured_messages.sh
 
 message "Install Avahi Aliases Manager" "header"
 for file in `ls ./package/*`;do
 	message "${file}"
-	sudo chmod 777 ${file}
-	sudo chmod +x ${file}
-	sudo chown root:root ${file}
-	sudo cp ${file} /usr/bin/
+	chmod 777 ${file}
+	chmod +x ${file}
+	chown root:root ${file}
+	cp ${file} /usr/bin/
 done
 
 message "Making avahi-aliases list directory" "header"
 message "Use this directory to place individual lists of avahi-aliases."
-sudo mkdir /etc/avahi/aliases.d/
+mkdir /etc/avahi/aliases.d/
 
 message "Making central avahi-aliases file" "header"
-sudo touch /etc/avahi/aliases
+touch /etc/avahi/aliases
 
 message "Installation of Avahi-Aliases Done" "success"
 
